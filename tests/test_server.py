aa420b59d136a61bff3d9a45a0c04404bfdb35f0
Liam <hoangtua.vn@gmail.com>
2020-08-12 22:27:31 -0400

    add unit tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,5 +1,21 @@
-from server import __version__
+import pytest
+from server import __version__, app
 
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_home_page(client):
+    res = client.get('/')
+    assert "200 OK"         in res.status
+    assert b"_home page_"   in res.data
+
+def test_hello_page(client):
+    res = client.get('/hello')
+    assert "200 OK"         in res.status
+    assert b"hello world!"  in res.data
